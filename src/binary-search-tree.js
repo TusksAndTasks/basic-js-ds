8ad8e611e0f021e9dc63b9adbba2925986fe8914
TusksAndTasks <thedestroyerofworlds@mail.ru>
2021-10-19 02:10:11 +0300

    feat: added min,max,find,has

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,84 +1,115 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
- const { Node } = require('../extensions/list-tree.js');
+  
+const { Node } = require('../extensions/list-tree.js')
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 module.exports = class BinarySearchTree {
-
-  
-  constructor(){
-    this.activeRoot = null;
+  constructor() {
+    this.activeRoot = null
   }
-
   root() {
-    return this.activeRoot;
+    return this.activeRoot
   }
 
-  add( data ) {
-    this.activeRoot = addNewNode(this.activeRoot, data);
-   
-    function addNewNode(node, data){
+  add(data) {
+    this.activeRoot = addNewNode(this.activeRoot, data)
+    function addNewNode(node, data) {
 
-    if (!node) {
-      return new Node(data);
+      if (!node) {
+        return new Node(data)
+      }
+      
+      else if (data < node.data) {
+        node.left = addNewNode(node.left, data)
+      } 
+      else if (data > node.data) {
+        node.right = addNewNode(node.right, data)
+      }
+
+      else if (node.data === data) {
+        return node
+      }
+
+      return node
     }
-
-    else if (node.value < data){
-      this.activeRoot = addNewNode(node.left, data);
-    }
-
-    else if (data < node.value){
-      this.activeRoot = addNewNode(node.right, data);
-    }
-
-    else if (node.value === data){
-      return node;
-    }
-
-    return node;
-}
-
   }
 
   has(data) {
-    return searchWithin(this.activeRoot, data);
-
-    function searchWithin(node, data) {
+    return isNodeHere(this.activeRoot, data)
+    function isNodeHere(node, data) {
       if (!node) {
-        return false;
+        return false
       }
 
       if (node.data === data) {
-        return true;
+        return true
       }
 
-      return data < node.data ? 
-        searchWithin(node.left, data) : 
-        searchWithin(node.right, data);
+      if (data < node.data) {
+        return isNodeHere(node.left, data)
+      } else {
+        return isNodeHere(node.right, data)
+      }
     }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findNode(this.activeRoot, data)
+    function findNode(node, data) {
+      if (!node) {
+        return null
+      }
+
+      if (node.data === data) {
+        return node
+      }
+
+      if (data < node.data) {
+        return findNode(node.left, data)
+      } else {
+        return findNode(node.right, data)
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let leftTracker = this.activeRoot
+    
+    if (leftTracker === null){
+      return null;
+    }
+   
+    let searchMinimalValue = (leftTracker) =>{
+      if (leftTracker.left === null){
+        return leftTracker.data
+      } else{
+        return searchMinimalValue(leftTracker.left)
+      }
+}
+      return searchMinimalValue(leftTracker);
+    }
+
+  
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
+    let rightTracker = this.activeRoot
+    
+    if (rightTracker === null){
+      return null;
+    }
+   
+    let searchMaximalValue = (rightTracker) =>{
+      if (rightTracker.right === null){
+        return rightTracker.data
+      } else{
+        return searchMaximalValue(rightTracker.right)
+      }
+}
+      return searchMaximalValue(rightTracker);
+    }
 }
